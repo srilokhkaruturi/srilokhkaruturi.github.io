32c115cdb7b0adb4b6d611c6e2762f8c1a077d12
virinchiracherla <v.racherla2@gmail.com>
2022-04-28 17:59:34 -0500

    change factorial.js

--- factorial.js
+++ factorial.js
@@ -12,4 +12,37 @@ function factorialize(num) {
         return num * factorialize(num-1);
     }
    
-  }
+}
+
+
+function getCreatorOne(){
+    return "Srilokh Karuturi (SXK180163)";
+}
+
+
+
+
+function getCreatorTwo(){
+    return "Virinchi Racherla(VRR180001)";
+}
+
+function changeButtonColor() {
+   // document.body.style.background.color = "purple";
+   document.body.style.backgroundColor = "purple";
+    return false;
+}  
+
+//<script src="factorial.js"></script>
+
+function changeButtonResetColor() {
+    // document.body.style.background.color = "purple";
+    document.body.style.backgroundColor = "white";
+     return false;
+ }  
+
+
+ function processIntro(){
+     if (document.getElementById('introButton').value == "See Intro"){
+        return "This program demonstrates proficiency in HTML, CSS, JS.";
+     }
+ }
